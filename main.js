abfba202e54580ee456ad4675cb9be76317f82cf
amandaghassaei <amandaghassaei@gmail.com>
2017-04-02 16:20:48 -0400

    reworking gpu stuff

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 //used a lot of ideas from https://bl.ocks.org/robinhouston/ed597847175cf692ecce to clean this code up
 
-var gl;
-var canvas;
-var frameBuffers;
-var states = [null, null];
-
-var resizedLastState;
-var resizedCurrentState;
-
-var width;
-var height;
-
-var stepProgram;
-var renderProgram;
-
-var textureSizeLocation;
-var textureSizeLocationRender;
+var width, height;
 
 var mouseCoordLocation;
 var mouseCoordinates =  [null, null];
@@ -23,6 +8,8 @@ var mouseEnableLocation;
 var mouseEnable = false;
 
 var paused = false;//while window is resizing
+
+var GPU;
 
 window.onload = initGL;
 
@@ -33,10 +20,7 @@ function initGL() {
         $("#aboutModal").modal('show');
     });
 
-    // Get A WebGL context
     canvas = document.getElementById("glcanvas");
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
 
     canvas.onmousemove = onMouseMove;
     canvas.onmousedown = onMouseDown;
@@ -45,125 +29,31 @@ function initGL() {
 
     window.onresize = onResize;
 
-    gl = canvas.getContext("webgl", {antialias:false}) || canvas.getContext("experimental-webgl", {antialias:false});
-    if (!gl) {
-        alert('Could not initialize WebGL, try another browser');
-        return;
-    }
+    GPU = initGPUMath();
 
-    gl.disable(gl.DEPTH_TEST);
-    gl.getExtension('OES_texture_float');
-
-    // setup a GLSL program
-    stepProgram = createProgramFromScripts(gl, "2d-vertex-shader", "2d-fragment-shader");
-    renderProgram = createProgramFromScripts(gl, "2d-vertex-shader", "2d-render-shader");
-    gl.useProgram(renderProgram);
-    loadVertexData(gl, renderProgram);
-    textureSizeLocationRender = gl.getUniformLocation(renderProgram, "u_textureSize");
-    gl.useProgram(stepProgram);
-    loadVertexData(gl, stepProgram);
-
-
-    textureSizeLocation = gl.getUniformLocation(stepProgram, "u_textureSize");
-    mouseCoordLocation = gl.getUniformLocation(stepProgram, "u_mouseCoord");
-    mouseEnableLocation = gl.getUniformLocation(stepProgram, "u_mouseEnable");
-
-    frameBuffers = [makeFrameBuffer(), makeFrameBuffer()];
+    // setup a GLSL programs
+    GPU.createProgram("advect", "2d-vertex-shader", "advectShader");
+    GPU.createProgram("render", "2d-vertex-shader", "2d-render-shader");
 
     resetWindow();
 
-    gl.bindTexture(gl.TEXTURE_2D, states[0]);//original texture
+    GPU.initFrameBufferForTexture("velocities");
 
     render();
-}
-
-function loadVertexData(gl, program) {
-	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ -1,-1, 1,-1, -1, 1, 1, 1]), gl.STATIC_DRAW);
-
-    // look up where the vertex data needs to go.
-	var positionLocation = gl.getAttribLocation(program, "a_position");
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-}
-
-function makeFrameBuffer(){
-    return gl.createFramebuffer();
-}
-
-function makeRandomArray(rgba){
-    for (var x=0;x<width;x++) {
-        for (var y=0;y<height;y++) {
-            var ii = (y*width + x) * 4;
-			var central_square = (x > width/2-10 && x < width/2+10 && y > height/2-10 && y < height/2+10);
-			if (central_square) {
-				rgba[ii] = 0.5 + Math.random() * 0.02 - 0.01;
-				rgba[ii + 1] = 0.25 + Math.random() * 0.02 - 0.01;
-			} else {
-				rgba[ii] = 1.0;
-				rgba[ii + 1] = 0;
-			}
-        }
-    }
-    return rgba;
-}
-
-function makeTexture(gl, data){
-
-    var texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    // Set the parameters so we can render any size image.
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, data);
-
-    return texture;
 }
 
 function render(){
 
     if (!paused) {
 
-        gl.useProgram(stepProgram);
-
-        if (mouseEnable){
-            gl.uniform1f(mouseEnableLocation, 1);
-            gl.uniform2f(mouseCoordLocation, mouseCoordinates[0], mouseCoordinates[1]);
-        } else gl.uniform1f(mouseEnableLocation, 0);
-
-        if (resizedLastState) {
-            states[0] = resizedLastState;
-            gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffers[0]);
-            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, states[0], 0);
-            resizedLastState = null;
-        }
-        if (resizedCurrentState) {
-            states[1] = resizedCurrentState;
-            gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffers[1]);
-            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, states[1], 0);
-            resizedCurrentState = null;
-        }
-
-        for (var i=0;i<2;i++) {
-            if (paused) {
-                window.requestAnimationFrame(render);
-                return;
-            }
-
-            step(i);
-        }
 
 
-        gl.useProgram(renderProgram);
+        // if (mouseEnable){
+        //     gl.uniform1f(mouseEnableLocation, 1);
+        //     gl.uniform2f(mouseCoordLocation, mouseCoordinates[0], mouseCoordinates[1]);
+        // } else gl.uniform1f(mouseEnableLocation, 0);
 
-        //draw to canvas
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-        gl.bindTexture(gl.TEXTURE_2D, states[0]);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        GPU.step("render", []);
 
     } else resetWindow();
 
@@ -187,20 +77,14 @@ function resetWindow(){
     width = canvas.clientWidth;
     height = canvas.clientHeight;
 
-    gl.viewport(0, 0, width, height);
+    GPU.setSize(width, height);
 
-    // set the size of the texture
-    gl.useProgram(stepProgram);
-    gl.uniform2f(textureSizeLocation, width, height);
-    gl.useProgram(renderProgram);
-    gl.uniform2f(textureSizeLocationRender, width, height);
 
-    //texture for saving output from frag shader
-    resizedCurrentState = makeTexture(gl, null);
+    GPU.setUniformForProgram("advect" ,"u_textureSize", [width, height], "2f");
 
-    //fill with random pixels
-    var rgba = new Float32Array(width*height*4);
-    resizedLastState = makeTexture(gl, makeRandomArray(rgba));
+
+    var velocities = new Float32Array(width*height*4);
+    GPU.initTextureFromData("velocities", width, height, "FLOAT", velocities, true);
 
     paused = false;
 }
