626bdec818efe0ee22b04f76593a83349eebea20
amandaghassaei <amandaghassaei@gmail.com>
2017-10-02 01:52:24 -0400

    default img

--- js/GPUMath.js
+++ js/GPUMath.js
@@ -12,6 +12,14 @@ function initGPUMath(){
     var floatTextures = gl.getExtension("OES_texture_float");
 
     function notSupported(){
+        var elm = '<div id="coverImg" ' +
+          'style="background: url(img.jpg) no-repeat center center fixed;' +
+            '-webkit-background-size: cover;' +
+            '-moz-background-size: cover;' +
+            '-o-background-size: cover;' +
+            'background-size: cover;">'+
+          '</div>';
+        $(elm).appendTo(body);
         $("#noSupportModal").modal("show");
        console.warn("floating point textures are not supported on your system");
     }
